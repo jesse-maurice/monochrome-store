ee3deaa9aaa1243ac5abf3a0975b9415ff5e8c66
jesse-maurice <iyohajessy@gmail.com>
2024-10-19 08:01:01 +0100

    Initial commit: set up react with vite, created navbar

---
+++ frontend/src/App.jsx
@@ -0,0 +1,36 @@
+import {
+  Route,
+  Routes,
+} from 'react-router-dom';
+
+import About from './pages/About';
+import Cart from './pages/Cart';
+import Collection from './pages/Collection';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Orders from './pages/Orders';
+import PlaceOrder from './pages/PlaceOrder';
+import Product from './pages/Product';
+
+function App() {
+
+  return (
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/product/:productId" element={<Product />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/place-order" element={<PlaceOrder />} />
+        <Route path="/collection" element={<Collection />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
